6841755e15004da7147e61a48b7aabfa876c4c32
Aleksey Kopasov <alesey.k1@gmail.com>
2020-05-11 14:47:21 +0300

    WIP: добавбляет сборку через webpack

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,11 @@
-import gulp from 'gulp';
-import svgstore from 'gulp-svgstore';
-import rename from 'gulp-rename';
-
-// SVG-sprite
-gulp.task('sprite', () => gulp.src('src/assets/img/sprite/*.svg')
-.pipe(svgstore({
-	inlineSvg: true,
-}))
-.pipe(rename('sprite.svg'))
-.pipe(gulp.dest('src/assets/img')));
+// import gulp from 'gulp';
+// import svgstore from 'gulp-svgstore';
+// import rename from 'gulp-rename';
+//
+// // SVG-svg-sprite
+// gulp.task('svg-sprite', () => gulp.src('src/assets/img/svg-sprite/*.svg')
+// .pipe(svgstore({
+// 	inlineSvg: true,
+// }))
+// .pipe(rename('svg-sprite.svg'))
+// .pipe(gulp.dest('src/assets/img')));
--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ import './js/'
 
 // SCSS
 import './assets/sass/style.scss'
+
+// SVG sprite
+function requireAll(r) {
+	r.keys().forEach(r);
+	console.log(r.keys());
+}
+
+requireAll(require.context('./assets/img/svg-sprite/', true, /\.svg$/));
+
